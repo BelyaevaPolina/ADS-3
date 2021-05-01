83f4b7c4d4a608f487c55455a8da7c8ad6ad4755
BelyaevaPolina <79137616+BelyaevaPolina@users.noreply.github.com>
2021-05-02 01:08:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,9 @@
 // Copyright 2021 NNTU-CS
 #include <string>
 #include "tstack.h"
-int priority(char ch_inf) {
-    switch (ch_inf) {
+
+int priority(char ch) {
+    switch (ch) {
     case '(': return 0;
     case ')': return 1;
     case '+': return 2;
@@ -12,32 +13,20 @@ int priority(char ch_inf) {
     default: return -1;
     }
 }
-int calculator(int num1, int num2 , char ch_pst) {
-    switch (ch_pst) {
-    case '+': return num1 + num2;
-    case '-': return num1 - num2;
-    case'*': return num1 * num2;
-    case '/': return num1 / num2;
-    }
-}
 std::string infx2pstfx(std::string inf) {
     char top = 0;
     TStack <char> stack1;
     std::string str_pst;
-
     for (int i = 0; i < inf.length(); i++) {
-
-        char ch = inf[i];
         int prior;
-        prior = priority(ch);
-
+        prior = priority(inf[i]);
         if (prior > -1) {
             if ((prior == 0 || prior > priority(top) ||
-                stack1.isEmpty()) && ch != ')') {
+                stack1.isEmpty()) && inf[i] != ')') {
                 if (stack1.isEmpty())
-                    top = ch;
-                stack1.push(ch);
-            } else if (ch == ')') {
+                    top = inf[i];
+                stack1.push(inf[i]);
+            } else if (inf[i] == ')') {
                 while (stack1.get() != '(') {
                     str_pst.push_back(stack1.get());
                     str_pst.push_back(' ');
@@ -58,7 +47,7 @@ std::string infx2pstfx(std::string inf) {
                 stack1.push(inf[i]);
             }
         } else {
-            str_pst.push_back(ch);
+            str_pst.push_back(inf[i]);
             str_pst.push_back(' ');
         }
     }
@@ -70,28 +59,37 @@ std::string infx2pstfx(std::string inf) {
     str_pst.erase(str_pst.end() - 1, str_pst.end());
     return str_pst;
 }
+int calculator(int num1, int num2, char operation) {
+    switch (operation) {
+    case '+': return num1 + num2;
+    case '-': return num1 - num2;
+    case'*': return num1 * num2;
+    case '/': return num1 / num2;
+    }
+}
 int eval(std::string pst) {
     TStack <int> stack2;
-    int i = 0, res = 0;
+    int i = 0;
+    int res = 0;
     char ch = pst[i];
     while (ch) {
         if (ch >= '0' && ch <= '9') {
-            int num = 0;
+            int ch_pst = 0;
             int dec = 1;
             while (ch != ' ') {
-                num += (ch - 48) * dec;
+                ch_pst += (ch - 48) * dec;
                 dec *= 10;
                 ch = pst[++i];
             }
-            stack2.push(num);
+            stack2.push(ch_pst);
         } else {
-            char ch_pst = ch;
+            char operation = ch;
             i++;
             int num2 = stack2.get();
             stack2.pop();
             int num1 = stack2.get();
             stack2.pop();
-            int result = calculator(num1, num2, ch_pst);
+            int res = calculator(num1, num2, operation);
             stack2.push(res);
         }
         if (i < pst.size())
