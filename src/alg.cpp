6ec694b284935a0182dd4d4d52050c43ef32f26b
BelyaevaPolina <79137616+BelyaevaPolina@users.noreply.github.com>
2021-05-01 19:57:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,33 +1,26 @@
 // Copyright 2021 NNTU-CS
 #include <string>
 #include "tstack.h"
-
-int priority(char ch_inf)
-{
-    switch (ch_inf)
-    {
-    case '(':return 0;
-    case ')':return 1;
-    case '+':return 2;
-    case '-':return 2;
-    case '*':return 3;
-    case '/':return 3;
-    default:return -1;
+int priority(char ch_inf) {
+    switch (ch_inf) {
+    case '(': return 0;
+    case ')': return 1;
+    case '+': return 2;
+    case '-': return 2;
+    case '*': return 3;
+    case '/': return 3;
+    default: return -1;
     }
 }
-
 int calculator(int num1, int num2 , char ch_pst) {
-    switch (ch_pst)
-    {
+    switch (ch_pst) {
     case '+': return num1 + num2;
     case '-': return num1 - num2;
     case'*': return num1 * num2;
     case '/': return num1 / num2;
     }
 }
-
-std::string infx2pstfx(std::string inf)
-{
+std::string infx2pstfx(std::string inf) {
     char top = 0;
     TStack <char> stack1;
     std::string str_pst;
@@ -44,8 +37,7 @@ std::string infx2pstfx(std::string inf)
                 if (stack1.isEmpty())
                     top = ch;
                 stack1.push(ch);
-            }
-            else if (ch == ')') {
+            } else if (ch == ')') {
                 while (stack1.get() != '(') {
                     str_pst.push_back(stack1.get());
                     str_pst.push_back(' ');
@@ -54,8 +46,7 @@ std::string infx2pstfx(std::string inf)
                 stack1.pop();
                 if (stack1.isEmpty())
                     top = 0;
-            }
-            else {
+            } else {
                 while (!stack1.isEmpty() &&
                     priority(stack1.get()) >= prior) {
                     str_pst.push_back(stack1.get());
@@ -66,8 +57,7 @@ std::string infx2pstfx(std::string inf)
                     top = inf[i];
                 stack1.push(inf[i]);
             }
-        }
-        else {
+        } else {
             str_pst.push_back(ch);
             str_pst.push_back(' ');
         }
@@ -80,8 +70,6 @@ std::string infx2pstfx(std::string inf)
     str_pst.erase(str_pst.end() - 1, str_pst.end());
     return str_pst;
 }
-
-
 int eval(std::string pst) {
     TStack <int> stack2;
     int i = 0, res = 0;
@@ -96,8 +84,7 @@ int eval(std::string pst) {
                 ch = pst[++i];
             }
             stack2.push(num);
-        }
-        else {
+        } else {
             char ch_pst = ch;
             i++;
             int num2 = stack2.get();
